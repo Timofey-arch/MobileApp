7b5cf144fde7a891550557a0472539a1c397f213
Bruh <timofejbakurov@gmail.com>
2023-05-20 13:20:44 +0700

    Добавлено руководство, план монетизации и карты

--- components/Pages/Home.js
+++ components/Pages/Home.js
@@ -4,7 +4,15 @@ export default function Home(){
     return(
         <>
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Home</Text>
+                <Text>Marketplaces to buy food</Text>
+                <MapView
+                    initialRegion={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                />  
             </View>
         </>
     );
